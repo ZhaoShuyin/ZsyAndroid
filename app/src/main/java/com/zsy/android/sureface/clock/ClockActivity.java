eea76691fb17e7f2c0d5d32aaa177341a9cf67c8
unknown <zhaoshuyin89@163.com>
2019-10-29 18:03:39 +0800

    abc

--- app/src/main/java/com/zsy/android/sureface/clock/ClockActivity.java
+++ app/src/main/java/com/zsy/android/sureface/clock/ClockActivity.java
@@ -74,23 +74,23 @@ public class ClockActivity extends Activity {
 		public void run() {
 			int count = 0;
 			while (isRun) {
-				Canvas c = null;
+				Canvas canvas = null;
 				try {
 					synchronized (holder) {
-						c = holder.lockCanvas();// 锁定画布，一般在锁定后就可以通过其返回的画布对象Canvas，在其上面画图等操作了。
-						c.drawColor(Color.BLACK);// 设置画布背景颜色
+						canvas = holder.lockCanvas();// 锁定画布，一般在锁定后就可以通过其返回的画布对象Canvas，在其上面画图等操作了。
+						canvas.drawColor(Color.BLACK);// 设置画布背景颜色
 						Paint p = new Paint(); // 创建画笔
 						p.setColor(Color.WHITE);
 						Rect r = new Rect(100, 50, 300, 250);
-						c.drawRect(r, p);
-						c.drawText("这是第" + (count++) + "秒", 100, 310, p);
+						canvas.drawRect(r, p);
+						canvas.drawText("这是第" + (count++) + "秒", 100, 310, p);
 						Thread.sleep(1000);// 睡眠时间为1秒
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				} finally {
-					if (c != null) {
-						holder.unlockCanvasAndPost(c);// 结束锁定画图，并提交改变。
+					if (canvas != null) {
+						holder.unlockCanvasAndPost(canvas);// 结束锁定画图，并提交改变。
 
 					}
 				}
